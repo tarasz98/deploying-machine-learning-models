8d4814a5e99dea66c632a1dfd57b2b00b23fd0f1
tarasz98 <tarasz98@gmail.com>
2023-09-04 15:56:32 +0200

    Fixed imports

--- packages/neural_network_model/neural_network_model/model.py
+++ packages/neural_network_model/neural_network_model/model.py
@@ -1,7 +1,7 @@
 # for the convolutional network
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten
-from keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam
 from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
 from keras.wrappers.scikit_learn import KerasClassifier
 
